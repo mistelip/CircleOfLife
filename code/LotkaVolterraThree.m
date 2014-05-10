13a81cd224d572784430211e742ab25913df928d
rukaelin <rukaelin@student.ethz.ch>
2014-05-10 19:11:14 +0200

    LotkaVolterraThree Parameterized
    continued report

--- code/LotkaVolterraThree.m
+++ code/LotkaVolterraThree.m
@@ -1,14 +1,19 @@
 function [] = LotkaVolterraThree()
+X_START = 300;
+WAVELENGTH = 200;
+ENDTIME = 3*WAVELENGTH/(WAVELENGTH/50);
 % timestep
-dt=0.001;
+dt=0.00006*WAVELENGTH;
 % iterations
-iter=10000;
+iter=333*ENDTIME;
 
 % initialize vectors
 x=zeros(iter,1); y=zeros(iter,1); z=zeros(iter,1); t=zeros(iter,1);
-x(1)=.5; y(1)=1; z(1)=2; t(1)=0; % starting point
+l = 0.05/WAVELENGTH;
 
-a=1; b=1; c=1; d=1; e=1; f=1; g=1;
+x(1)=1*X_START; y(1)=0.1*X_START; z(1)=0.1*X_START; t(1)=0; % starting point
+
+a=0.5*X_START*l; b=1*l; c=1*l; d=0.5*l; e=1*l; f=0.5*X_START*l; g=1*l;
 
 for i=2:iter
     dx=a*x(i-1)- b*x(i-1)*y(i-1);
@@ -19,11 +24,15 @@ for i=2:iter
     z(i)=z(i-1)+dt*dz;
     t(i)=t(i-1)+dt;
 end
-plot(x);
+
+color_Lion = [1 0 0];
+color_Antelope = [244/255, 164/255, 96/255];
+color_Grass = [0 1 0];
+
+plot(t,x,'color',color_Grass);
 hold on;
-plot(y,'r');
-plot(z,'g');
+plot(t,y,'color',color_Antelope);
+plot(t,z,'color',color_Lion);
 legend('grass','antilopes','lions');
-%plot3(x,y,z);
 end
 
