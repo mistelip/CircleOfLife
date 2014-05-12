2723f52fc1b67cf88b0545d1442b2e4a025a1373
rukaelin <rukaelin@student.ethz.ch>
2014-05-12 17:08:33 +0200

    figures from circle of life simulations

--- code/LotkaVolterraThree.m
+++ code/LotkaVolterraThree.m
@@ -7,20 +7,24 @@ color_Grass = [0 1 0];
 FONTSIZE = 15;
 
 X_START = 300;
-WAVELENGTH = 50;
-ENDTIME = 100;
+WAVELENGTH = 35;
+ENDTIME = 132;
 % timestep
 dt=0.00006*WAVELENGTH;
 % iterations
-iter=333*ENDTIME;
+iter=ceil(333*ENDTIME*(50/WAVELENGTH));
 
 % initialize vectors
 x=zeros(iter,1); y=zeros(iter,1); z=zeros(iter,1); t=zeros(iter,1);
 l = 0.05/WAVELENGTH;
 
-x(1)=1*X_START; y(1)=0.1*X_START*0; z(1)=0.1*X_START; t(1)=0; % starting point
+x(1)=1*X_START; y(1)=0.1*X_START; z(1)=0.1*X_START; t(1)=0; % starting point
 
 a=0.5*X_START*l; b=1*l; c=1*l; d=0.5*l; e=1*l; f=0.5*X_START*l; g=1*l;
+
+% all ones
+%x(1) = 0.5;y(1) = 1; z(1) =2;
+%a =1;b=1;c=1;d=1;e=1;f=1;g=1;
 
 for i=2:iter
     dx=a*x(i-1)- b*x(i-1)*y(i-1);
@@ -33,10 +37,10 @@ for i=2:iter
 end
 
 %hold on;
-semilogy(t,x,'LineWidth',2,'Color',color_Grass);
+plot(t,x,'LineWidth',2,'Color',color_Grass);
 hold on;
-semilogy(t,y,'LineWidth',2,'Color',color_Antelope);
-semilogy(t,z,'LineWidth',2,'Color',color_Lion);
+plot(t,y,'LineWidth',2,'Color',color_Antelope);
+plot(t,z,'LineWidth',2,'Color',color_Lion);
 %Legend
 legend('grass','antilopes','lions');
 legend(gca,'show');
@@ -52,6 +56,8 @@ set(xlab,'FontSize',FONTSIZE);
 set(ylab,'FontSize',FONTSIZE);
 set(gca, 'XColor', 'w');
 set(gca, 'YColor', 'w');
+
+%xlim([0,ENDTIME]);
 
 set(gca,'XColor',[0,0,0]);
 set(gca,'YColor',[0,0,0]);
